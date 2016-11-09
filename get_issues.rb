68ab9d3e68523098f593d3989b6f737cf19c2eb8
noamdeul <noamd@il.ibm.com>
2016-11-09 16:40:25 +0200

    updated html and css

--- get_issues.rb
+++ get_issues.rb
@@ -3,30 +3,37 @@ require 'json'
 
 
 def get_issues
-      query = { project_id: 1 }
-      headers = {
-        'X-Redmine-API-Key' => '1bf27c0e69064e411e64a162850b9b101da6621f'
-      }
+  query = { project_id: 1 }
+  headers = {
+    'X-Redmine-API-Key' => '1bf27c0e69064e411e64a162850b9b101da6621f'
+  }
 
-      response = HTTParty.get(
-        "http://46.101.169.41/issues.json",
-        :query => query,
-        :headers => headers
-      )
+  response = HTTParty.get(
+    "http://46.101.169.41/issues.json",
+    :query => query,
+    :headers => headers
+  )
 
-      if response.code == 200
-        response["issues"]
-      else
-        []
-      end
+  if response.code == 200
+    response["issues"]
+  else
+    []
+  end
 
-    end
+end
 
-    #puts get_issues
+#puts get_issues
 
-    issues = get_issues.map { |r| { 'id' => r['id'] ,"subject" => r["subject"], "status" => r["status"]["name"], "created_on" => r["created_on"]} }
-
-    # open and write to a file with ruby
+issues = get_issues.map do |r|
+  {
+    'id' => r['id'] ,
+    "subject" => r["subject"],
+    "status" => r["status"]["name"],
+    "created_on" => Time.parse(r["created_on"]).strftime("%d-%m-%Y"),#.to_date,
+    "days_open" => "#{(Time.now.to_date - Time.parse(r["created_on"]).to_date).round}" #r["created_on"]
+  }
+end
+# open and write to a file with ruby
 open('issues.json', 'w') { |f|
   f.puts "data = '" + issues.to_json + "';"
 }
